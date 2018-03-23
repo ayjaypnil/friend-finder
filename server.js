61ea7da140e74024a61d81061105880db58830b3
Anil Patel <ajpatel92@icloud.com>
2018-03-23 16:19:06 -0500

    port configs again

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require("path");
 
 
 var app = express();
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
